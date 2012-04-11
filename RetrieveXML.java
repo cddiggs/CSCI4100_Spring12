85e0b5554b2ebd7bdde4b9ca6f7674c6b66e53fc
Derek Lathan <dereklathan@gmail.com>
2012-04-11 15:30:25 -0500

    removed static keywords and main method from RetrieveXML.java

--- RetrieveXML.java
+++ RetrieveXML.java
@@ -25,7 +25,7 @@ public class RetrieveXML {
 	 * @param section
 	 * @return List of integer values of questions that are of the given difficulty
 	 */
-	public static List returnBySection(double section){
+	public List returnBySection(double section){
 		List questionsToReturn = new List();
 		try {
 			DocumentBuilderFactory dBF = DocumentBuilderFactory.newInstance();
@@ -64,7 +64,7 @@ public class RetrieveXML {
 	 * @param sectionTopic
 	 * @return List of integer values of questions that are of the given difficulty
 	 */
-	public static List returnByTopic(String sectionTopic){
+	public List returnByTopic(String sectionTopic){
 		List questionsToReturn = new List();
 		try {
 			DocumentBuilderFactory dBF = DocumentBuilderFactory.newInstance();
@@ -103,7 +103,7 @@ public class RetrieveXML {
 	 * @param difficulty
 	 * @return List of integer values of questions that are of the given difficulty
 	 */
-	public static List returnByDifficulty(int difficulty){
+	public List returnByDifficulty(int difficulty){
 		List questionsToReturn = new List();
 		try {
 			DocumentBuilderFactory dBF = DocumentBuilderFactory.newInstance();
@@ -143,7 +143,7 @@ public class RetrieveXML {
 	 * @param tagname
 	 * @return String with the requested test data
 	 */
-	public static String returnTestData(int questionID, String tagname) {
+	public String returnTestData(int questionID, String tagname) {
 		String returnvalue = "";
 		try {
 			DocumentBuilderFactory dBF = DocumentBuilderFactory.newInstance();
@@ -175,30 +175,5 @@ public class RetrieveXML {
 		}		
 		return returnvalue;
 	}
-	
 
-	
-	
-	public static void main(String args[]) {
-		double requestedSection = 3.4;
-		int requestedDifficulty = 5;
-		String requestedFormat = "latex_q";
-		
-		String requestMethod = "section"; //default request method
-		
-		/**
-		 * List that holds the value of a returnBy function call
-		 */
-		List returnedQuestions;
-		
-		if (requestMethod == "section")
-			returnedQuestions = returnBySection(requestedSection);
-		else //if (requestMethod == "difficulty")
-			returnedQuestions = returnByDifficulty(requestedDifficulty);
-		
-		for (int i = 0; i < returnedQuestions.getItemCount(); i++) {
-			System.out.println(returnedQuestions.getItem(i));
-			System.out.println(returnTestData(Integer.valueOf(returnedQuestions.getItem(i)), requestedFormat));
-		}
-	}
 }
