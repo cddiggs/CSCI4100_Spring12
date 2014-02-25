2e27ef3280f5ea6046c75594157d1ea47db0bd04
Chris Diggs <cddiggs082990@yahoo.com>
2014-02-25 13:22:44 -0600

    example

--- menu.java
+++ menu.java
@@ -1,72 +1,265 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class menu {
-	
-	public static void main(String args[]) throws IOException, InterruptedException {
-	
-        Scanner scan = new Scanner(System.in);
-            
-        System.out.println("TEST GENERATION PROGRAM");
-		System.out.println("Enter database name:");
-		String file = scan.nextLine();
 
-		System.out.println("Select type of test to create:");
-		System.out.println("A. LaTeX/PDF");
-		System.out.println("B. Web");
-		String test = scan.nextLine();
-		
-        System.out.println("Enter test name (alphanumeric characters and spaces only)");
-        String testname = scan.nextLine();
-        String useablename = testname;
+public class menu {
+    public static Scanner scan = new Scanner(System.in);
+    public static String testname, file;
+    public static String[] databaseList; 
+
+
+	public static void main(String args[]) throws InterruptedException, IOException {
+        System.out.println("Would you like to use:");
+        System.out.println("a. GUI Menu");
+        System.out.println("b. Command Line Menu:");
+    	String option = scan.nextLine();
+    	
+    	if (option.charAt(0)=='a'||option.charAt(0)=='A'){
+    		GUIMenu menu = new GUIMenu();
+    	}
+    	else if (option.charAt(0)=='b'||option.charAt(0)=='B'){
+    		
+            commandOptions();
+    	}
+    	else         System.out.println("Invalid Option");
+
+	}
+	
+	public static void commandOptions() throws InterruptedException, IOException {
+        System.out.println("TEST GENERATION PROGRAM");
+		System.out.println("Please select an option to choose from:");
+		System.out.println("a. Make a PDF Test");
+		System.out.println("b. Make a HTML Test");
+		System.out.println("c. Make a Jeopardy Board");
+		System.out.println("d. Add or remove a Database from the List");
+		System.out.println("e. Add or edit questions in a Database");
+
+		String option = scan.nextLine();
+    	
+    	if (option.charAt(0)=='a'||option.charAt(0)=='A'){
+    		makeTest(true);//true to make pdf
+    	}
+    	else if (option.charAt(0)=='b'||option.charAt(0)=='B'){
+    		makeTest(false);//false to make html
+    	}
+    	else if (option.charAt(0)=='c'||option.charAt(0)=='C'){
+    		//makeJeopardBoard(): function to create jeopardy board here
+    	}
+    	else if (option.charAt(0)=='d'||option.charAt(0)=='D'){
+    		addOrRemoveDatabase();
+    	}
+    	else if (option.charAt(0)=='e'||option.charAt(0)=='E'){
+    		addOrEditQuestions();
+    	}
+    	else         System.out.println("Invalid Option");
+
+	}
+	public static void makeTest(boolean trueOrFalse) throws IOException, InterruptedException {
+        System.out.println("Please enter the name of the Test (alphanumeric characters and spaces only):");
+        testname = scan.nextLine();
         testname = testname.replaceAll(" ", "_");
 		
-        LatexFile database1 = null;
-		htmlFile database2 = new htmlFile(file);
-		boolean lbase = false; //true = Latex, false = HTML
-		boolean hbase = false; //true = HTML, false = Latex
-		if (test.charAt(0) == 'A' || test.charAt(0) == 'a') {
-			lbase = true;
-			hbase = false;
-		}
-		else if(test.charAt(0) == 'B' || test.charAt(0) == 'b') {
-			hbase = true;
-			lbase = false;
-		}
-		System.out.println("Enter subject covered");
-        String subject = scan.nextLine();
-        if(lbase==true){
-        	database1 = new LatexFile(testname + ".tex",file);
-        	database1.WriteLatexHead(useablename);
-        }
-		boolean continuance = true;
-		while (continuance){
-			System.out.println("Enter sections to print");
-			Double section = scan.nextDouble();
-			System.out.println("Enter question difficulties to print");
-			int difficulty = scan.nextInt();
-			System.out.println("Enter max number of questions to print");
-			int number = scan.nextInt();
-			if(lbase)
-				database1.WriteLatexQuestions(subject,section,difficulty,number);
-			if (hbase)
-				database2.WritehtmlQuestions(subject,section,difficulty,number);
-			System.out.println("Do you want to continue adding questions? Y = yes, N = no");
+		
+		System.out.println("Please select a Database from the list:");
+		BufferedReader reader = new BufferedReader(new FileReader("./src/database.txt"));
+		String line = null;
+		int i=1;
 
-            String resp = scan.next();
-			if (resp.charAt(0) == ('Y' | 'y'))
-				continuance = true;
-			else
-				continuance = false;
-		} 
-		System.out.println("Test file generated. Goodbye");
-		if(lbase==true) {
-			database1.WriteLatexFoot();
-			Runtime run = Runtime.getRuntime();
-			Process pr = run.exec("pdflatex " + testname + ".tex");
-			pr.waitFor();
+		databaseList = new String[20];
+		while ((line = reader.readLine()) != null) {
+			databaseList[i]=line;
+			System.out.println(i+ ": "+line);
+			i++;
 		}
-		if(hbase==true)
-			database2.GeneratehtmlTest(useablename);
+		int fileNumber;
+		fileNumber=scan.nextInt();
+		if (databaseList[fileNumber]== null){
+			System.out.println("Invalid Option");
+			System.exit(0);
+
+		}
+		else file=databaseList[fileNumber];
+		
+		System.out.println("Would you like to:");
+		System.out.println("a. Randomly select Questions from the Database?");
+		System.out.println("b. Specifically select Questions from the Database?");
+		
+		String option = scan.next();
+		  	if (option.charAt(0)=='a'||option.charAt(0)=='A'){
+	    		randomQuestions(trueOrFalse);
+	    	}
+		  	else if (option.charAt(0)=='b'||option.charAt(0)=='B'){
+    		specificQuestions(trueOrFalse);
+
+		  	}
+	  	
+		System.out.println("PDF File Generated!");
+		
+	}
+
+	public static void randomQuestions(boolean trueOrFalse){
+		if (trueOrFalse==true){
+		//execute function to add random questions via question IDs to pdf
+			}
+		else {//execute function to add random specific questions via question IDs to html
+			}
+	}
+		
+	public static void specificQuestions(boolean trueOrFalse) throws InterruptedException, IOException{
+		System.out.println("Would you like for the questions to be displayed in your default browser? (y/n)");
+		String option = scan.next();
+	  	if (option.charAt(0)=='y'||option.charAt(0)=='Y'){
+    		displayQuestions();
+    	}
+    	else {	}
+		System.out.println("Enter the number of questions you would like to add:");
+		int numberOfQuestions = scan.nextInt();
+		int[] questionIDs;
+		questionIDs = new int[numberOfQuestions];
+		System.out.println("Enter the ID numbers of the " +numberOfQuestions+ " questions you would like to add:");
+		for (int i=0;i<numberOfQuestions;i++){
+			questionIDs[i]=scan.nextInt();
+			}
+		if (trueOrFalse==true){
+		//execute function to add specific questions via question IDs to pdf
+			}
+		else {//execute function to add specific questions via question IDs to html
+			}
+		
+
+	}
+	public static void displayQuestions() throws IOException, InterruptedException {
+
+        LatexFile database1 = null;
+    	database1 = new LatexFile(testname + ".tex",file);
+    	database1.WriteLatexHead(testname);
+        database1.WriteAllLatexQuestions();
+		database1.WriteLatexFoot();
+		
+		
+        htmlFile database2 = null;
+    	database2 = new htmlFile(file);
+    	database2.WriteAllhtmlQuestions();
+    	database2.GenerateAllHtml(testname);
+
+    	Process p;
+        p = Runtime.getRuntime().exec("firefox " + "./" + testname + "/index.html &");
+        p.waitFor();
+		
+}
+	
+	public static void addOrRemoveDatabase() throws IOException, InterruptedException{
+			System.out.println("Here is the current list of Databases:");
+			BufferedReader reader = new BufferedReader(new FileReader("./src/database.txt"));
+			String line = null;
+			int i=1;
+	
+			databaseList = new String[20];
+			while ((line = reader.readLine()) != null) {
+				databaseList[i]=line;
+				System.out.println(i+ ": "+line);
+				i++;
+			}
+			
+			System.out.println("Would you like to:");
+			System.out.println("a. Add a Database to the list?");
+			System.out.println("b. Remove a Database from the list?");
+			String option = scan.nextLine();
+			if (option.charAt(0)=='a'||option.charAt(0)=='A'){
+				System.out.println("Please type the path to the new Database you would like to add from the list:");
+				String databasePath=scan.nextLine();
+				BufferedWriter writer = new BufferedWriter(new FileWriter("./src/database.txt",true));
+			
+				writer.append(databasePath);
+				writer.newLine();
+				writer.close();
+				System.out.println("You have successfully added a new Database to the list:");
+				}
+		  	else if (option.charAt(0)=='b'||option.charAt(0)=='B'){
+				System.out.println("Please select a Database you would like to remove from the list");
+
+				int fileNumber=scan.nextInt();
+				if (databaseList[fileNumber]== null){
+					System.out.println("Invalid Option");
+				}
+				else{
+					BufferedReader reader2 = new BufferedReader(new FileReader("./src/database.txt"));
+					BufferedWriter writer = new BufferedWriter(new FileWriter("./src/tempDatabase.txt",true));
+
+					String lineToRemove = databaseList[fileNumber];
+					String currentLine;
+
+					while((currentLine = reader2.readLine()) != null) {
+					    // trim newline when comparing with lineToRemove
+					    String trimmedLine = currentLine.trim();
+					    if(trimmedLine.equals(lineToRemove)) continue;
+					    writer.write(currentLine);
+						writer.newLine();					
+						}	
+					writer.close();
+			    	Process p;
+			        p = Runtime.getRuntime().exec("mv ./src/tempDatabase.txt ./src/database.txt");
+			        p.waitFor();
+				}
+			
+			
+		  	}
+			else 		System.out.println("Invalid Option");
+	
+			}
+	
+	public static void addOrEditQuestions() throws IOException, InterruptedException{
+		System.out.println("Please select a Database from the list:");
+		BufferedReader reader = new BufferedReader(new FileReader("./src/database.txt"));
+		String line = null;
+		int i=1;
+
+		databaseList = new String[20];
+		while ((line = reader.readLine()) != null) {
+			databaseList[i]=line;
+			System.out.println(i+ ": "+line);
+			i++;
+		}
+		int fileNumber;
+		fileNumber=scan.nextInt();
+		if (databaseList[fileNumber]== null){
+			System.out.println("Invalid Option");
+			System.exit(0);
+
+		}
+		
+		
+		System.out.println("Would you like to:");
+		System.out.println("a. Add a question to the Database?");
+		System.out.println("b. Edit a quesion from the Database?");
+		System.out.println("c. Remove a quesion from the Database?");
+
+		String option = scan.next();
+		if (option.charAt(0)=='a'||option.charAt(0)=='A'){
+			//executes function to add blank tags in xml
+			//executes function to edit the blank tags
+		}
+		else if (option.charAt(0)=='b'||option.charAt(0)=='B'){
+			System.out.println("Would you like for the questions to be displayed in the Firefox browser? (y/n)");
+			String option2 = scan.next();
+		  	if (option2.charAt(0)=='y'||option2.charAt(0)=='Y'){
+		  		testname="display";
+		  		file=databaseList[fileNumber];
+	    		displayQuestions();
+	    	}
+	    	else {	}
+			//executes function to edit tags of an existing question, refer to question by ID
+		}
+		else if (option.charAt(0)=='c'||option.charAt(0)=='C'){
+			//executes function to remove entire question from database
+		}
+		else 		System.out.println("Invalid Option");
+
 	}
 }
+
