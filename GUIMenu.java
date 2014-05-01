9cfec9dc91c479fddf3c8e998667e86ae0c42bbc
alonzathomas <alonzathomas@gmail.com>
2014-05-01 06:41:05 -0500

    Updated GUIMenu implementation to use the new items

--- GUIMenu.java
+++ GUIMenu.java
@@ -1,164 +1,183 @@
-
+import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.*;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.GridLayout;
-import java.awt.event.*;
 
-
-public class GUIMenu extends JFrame {
-	
-	private JFrame frame=new JFrame("Test Generator");
-	
-	private JMenuBar menuBar = new JMenuBar();
-
-	
-	private JMenu File = new JMenu("File");
-
-	private JMenu Edit = new JMenu("Edit");
-	private JMenu Help = new JMenu("Help");
-
-	private JMenu New= new JMenu("New");
-	private JMenuItem Lpdf= new JMenuItem("Latex\\PDF Test");
-	private JMenuItem HTML= new JMenuItem("HTML Test");
-	private JMenuItem JB= new JMenuItem("Jepardy Board");
-	private JMenuItem Exit = new JMenuItem("Exit");
-	private JMenuItem About = new JMenuItem("About");
-
-	private JPanel lpdfpanel = new JPanel();
-	
-	private JLabel lblPdfTest = new JLabel("This is a Test Generator and a Jeopardy Board Generator");
-	private JLabel lblsuccess = new JLabel("Test generation successful!");
-	private JLabel dpath = new JLabel("Enter the path of the database:");
-	private JLabel testname = new JLabel("Enter the test name:");
-	private JLabel subjectname = new JLabel("Enter the what subject you would like to use:");
-	private JLabel sectionname = new JLabel("Enter the what section you would like to use:");
-	private JLabel diffrange = new JLabel("Enter the the difficulty:");
-	private JLabel numbofqs = new JLabel("Enter the number of questions you would like to add:");
-
-
-	private JButton nextbutton = new JButton("Next");
-	
-	private JPanel contentPane = new JPanel(new GridLayout());
-	private JTextField dtextfield = new JTextField("");
-	private JTextField tntextfield = new JTextField("");
-	private JTextField subtextfield = new JTextField("");
-	private JTextField secttextfield = new JTextField("");
-	private JTextField difftextfield = new JTextField("");
-	private JTextField qtextfield = new JTextField("");
-
-
-	private String testn;
-	private String databn;
-	private String subjectn;
-	private double sectn;
-	private int diffn;
-	private int questn;
+public class GUIMenu {
 	
 	
-	public GUIMenu() {
-
-		setupFrame();
-	}
-	private void setupFrame(){
-		frame.setSize(700,300);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setJMenuBar(menuBar);
-		frame.setResizable(false);
-		
-		menuBar.add(File);
-		menuBar.add(Edit);
-		menuBar.add(Help);
-		
-		
-
-		New.add(Lpdf);
-		New.add(HTML);
-		New.add(JB);
-		File.add(New);
-		File.addSeparator();
-		File.add(Exit);
-			Exit.addActionListener(new exitaction());	
-			Lpdf.addActionListener(new lpdfaction());
-			HTML.addActionListener(new lpdfaction());
-		Help.add(About);
-		
-		contentPane.setLayout(new GridLayout(13, 2));
-		contentPane.add(lblPdfTest);
-		frame.setContentPane(contentPane);
-	
-       }
-	
-	 class exitaction implements ActionListener{
-		public void actionPerformed (ActionEvent e){
-			System.exit(1);
-			
-		}
-	}
-	 class lpdfaction implements ActionListener{
-		public void actionPerformed (ActionEvent e){
-			removecomponents();
-			addcomponents();
-
-			nextbutton.addActionListener( new nextbutton1action());
-			
-			frame.setContentPane(contentPane);
-
-
-			
-		}
-	}
-	
-	public class nextbutton1action implements ActionListener{
-		public void actionPerformed (ActionEvent e){
-		 testn=tntextfield.getText();
-		 //System.out.println(testn);
-		 databn=dtextfield.getText();
-		subjectn=subtextfield.getText();
-		sectn=Double.parseDouble(secttextfield.getText());
-		diffn=Integer.parseInt(difftextfield.getText());
-		questn=Integer.parseInt(qtextfield.getText());
-		removecomponents();
-		contentPane.add(lblsuccess);
-		frame.setContentPane(contentPane);
-		
-		}
-	}
-	
-	public void addcomponents(){
-		contentPane.add(testname);
-		contentPane.add(tntextfield);
-		contentPane.add(dpath);
-		contentPane.add(dtextfield);
-		contentPane.add(subjectname);
-		contentPane.add(subtextfield);
-		contentPane.add(sectionname);
-		contentPane.add(secttextfield);
-		contentPane.add(diffrange);
-		contentPane.add(difftextfield);
-		contentPane.add(numbofqs);
-		contentPane.add(qtextfield);
-		contentPane.add(nextbutton);
-		
-	}
-	public void removecomponents (){
-		contentPane.remove(lblPdfTest);
-		contentPane.remove(lblsuccess);
-		contentPane.remove(testname);
-		contentPane.remove(tntextfield);
-		contentPane.remove(dpath);
-		contentPane.remove(dtextfield);
-		contentPane.remove(subjectname);
-		contentPane.remove(subtextfield);
-		contentPane.remove(sectionname);
-		contentPane.remove(secttextfield);
-		contentPane.remove(diffrange);
-		contentPane.remove(difftextfield);
-		contentPane.remove(numbofqs);
-		contentPane.remove(qtextfield);
-		contentPane.remove(nextbutton);
-	}
+    JTextArea output;
+    JScrollPane scrollPane;
+ 
+    /** creates the main menu bar*/
+    public JMenuBar createMenuBar() {
+        JMenuBar menuBar;
+        JMenu menu, submenu, menu2, submenu2;
+        JMenuItem menuItem, menuItem2;
+ 
+        /** Create the menu bar*/
+        menuBar = new JMenuBar();
+        /** File Menu */
+        JMenu file = new JMenu("File");
+        file.setMnemonic(KeyEvent.VK_F);
+         
+        /** Build the New Items submenu within File.*/
+        submenu = new JMenu("New");
+        submenu.setMnemonic(KeyEvent.VK_N);
+ 
+        /**a group of JMenuItems for the "New" Submenu*/
+ 
+        menuItem = new JMenuItem("PDF Test");
+        menuItem.setToolTipText("Create a new Test");
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_2, ActionEvent.ALT_MASK));
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {try {
+				GUI_PDFtest frame = new GUI_PDFtest();
+				frame.setVisible(true);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+            }
+        });
+        submenu.add(menuItem);
+        /** Menu item to create a new HTML test */
+        menuItem = new JMenuItem("HTML Test");
+        menuItem.setToolTipText("Create new HTML Test");
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {try {
+				GUI_HTMLtest frame = new GUI_HTMLtest();
+				frame.setVisible(true);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+            }
+        });
+        submenu.add(menuItem);
+        file.add(submenu);
+        /** Menu item to create a Jeopardy Board */
+        //TODO The object for the GUI Jeopardy Board needs to be created
+        menuItem = new JMenuItem("Jeopardy Board");
+        submenu.add(menuItem);
+        file.add(submenu);
+        
+        /** Exit Option */
+        JMenuItem eMenuItem = new JMenuItem("Exit");
+        eMenuItem.setMnemonic(KeyEvent.VK_E);
+        eMenuItem.setToolTipText("Exit application");
+        eMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+        file.add(eMenuItem);
+        //Add
+        menuBar.add(file);
+ 
+        /** Build the Editor Menu */
+        menu2 = new JMenu("Editor");
+        menu2.setMnemonic(KeyEvent.VK_A);
+        menuBar.add(menu2);
+ 
+        /**JMenuItems for the Editor menu2
+        //Database section*/
+        submenu2 = new JMenu("Database");
+        submenu2.setMnemonic(KeyEvent.VK_N);
+ 
+        menuItem2 = new JMenuItem("Add");
+        menuItem2.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_2, ActionEvent.ALT_MASK));
+        menuItem2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {try {
+				GUI_AddDatabase frame = new GUI_AddDatabase();
+				frame.setVisible(true);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+            }
+        });
+        submenu2.add(menuItem2);
+ 
+        menuItem2 = new JMenuItem("Remove");
+        menuItem2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {try {
+				GUI_RemoveDatabase frame = new GUI_RemoveDatabase();
+				frame.setVisible(true);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+            }
+        });
+        submenu2.add(menuItem2);
+        menu2.add(submenu2);
+        /**Questions Section*/
+        submenu2 = new JMenu("Questions");
+        submenu2.setMnemonic(KeyEvent.VK_N);
+ 
+        //TODO The object for GUI Add question needs to be created
+        menuItem2 = new JMenuItem("Add");
+        menuItem2.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_2, ActionEvent.ALT_MASK));
+        submenu2.add(menuItem2);
+ 
+        //TODO the object for GUI Remove question needs to be created
+        menuItem2 = new JMenuItem("Remove");
+        submenu2.add(menuItem2);
+        menu2.add(submenu2);
+        
+        //TODO the object for GUI edit question needs to be created
+        menuItem2 = new JMenuItem("Edit");
+        submenu2.add(menuItem2);
+        menu2.add(submenu2);
+        /**Creates the Menu bar on the main GUI window*/
+        return menuBar;
+    }
+ 
+    public Container createContentPane() {
+        /**Create the content in progress.*/
+        JPanel contentPane = new JPanel(new BorderLayout());
+        contentPane.setOpaque(true);
+ 
+        /**Create a scrolled text area.*/
+        output = new JTextArea(5, 30);
+        output.setEditable(false);
+        scrollPane = new JScrollPane(output);
+ 
+        /**Add the text area to the content pane.*/
+        contentPane.add(scrollPane, BorderLayout.CENTER);
+ 
+        return contentPane;
+    }
+ 
+    /**
+     * Create the GUI and show it.*/
+    static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Test Generation Program");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Create and set up the content pane.
+        GUIMenu demo = new GUIMenu();
+        frame.setJMenuBar(demo.createMenuBar());
+        frame.setContentPane(demo.createContentPane());
+ 
+        //Display the window.
+        frame.setSize(450, 260);
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+        //Create and Show the GUI
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 }
